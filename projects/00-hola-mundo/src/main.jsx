91795a9e818e59cdc1cf0ee5fa3ab1bbc9f5f832
Josue Suarez <jsuarez1670@gmail.com>
2024-09-03 12:34:30 -0500

    cambios random

--- projects/00-hola-mundo/src/main.jsx
+++ projects/00-hola-mundo/src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+//import { StrictMode } from "react";
+import reactDOM from "react-dom/client";
+import { App } from "./App.jsx";
+//import { createRoot } from "react-dom/client";
+//import App from "./App.jsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+import React from "react";
+
+const root = reactDOM.createRoot(document.getElementById("root"));
+/*
+<StrictMode>
     <App />
-  </StrictMode>,
-)
+
+</StrictMode>,
+
+*/
+
+root.render(<App />);
